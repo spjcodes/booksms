6e9bafd9d3d2f8bae57cc33f01e2459c01783e5d
17295 <1729539160@qq.com>
2019-03-30 13:24:01 +0800

    基础开环境搭建完成 bootstap 集成

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './pages/navbar/navbar.component';
+import { FooterComponent } from './pages/footer/footer.component';
+import { SearchComponent } from './pages/search/search.component';
+import { CarouselComponent } from './pages/carousel/carousel.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    SearchComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule
